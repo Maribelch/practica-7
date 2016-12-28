df00a6e5e3a4ae6485c24520aad56610cad5de53
maribel <you@example.com>
2016-12-28 13:00:10 -0500

    zare

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 function calcular(){
     var a = document.getElementById("a");
     var b =document.getElementById("b");
-    var b1=b.value;
-    var a1=a.value;
+    var negative=parseInt(document.getElementById("nega").value);
+    
+    var a=a.value;
+    var b=b.value;
     
     var salida =document.getElementById("salida");
-    
-    if(a1>0 && b1>0){
-        salida.innerHTML="0";
-    }
-    else{
-        salida.innerHTML="1";
+
+    if(( (a<0)&&(b>0) || (a>0)&&(b<0) )&& negative==0)
+        {
+            salida.innerHTML="1";
+          }
+    else
+        {
+        if ((negative==1)&&(a<0 && b<0))
+             salida.innerHTML="1";
+        else 
+             salida.innerHTML="0";
     }
 }
